897b63bad70221f98b20d54e68c6d4db2d783c9a
Levy <edgarlevy81@gmail.com>
2023-10-30 00:04:23 +0300

    Lab attempted (Failing)

--- lib/debug.py
+++ lib/debug.py
@@ -5,4 +5,40 @@ from song import Song
 
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; 
+class Song:
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
+hello = Song("Hello", "25")
+hello.save()
+
+despacito = Song("Despacito", "Vida")
+despacito.save()
+
+hello.id
+despacito.id
+ipdb.set_trace()
